850b01fc580fa0bd54ce1bbc46a065dd4c298bc6
Alexandre A Kupac <akupac@gmail.com>
2025-04-04 14:54:24 -0300

    Refactor Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,15 @@ import { defineConfig, loadEnv } from 'vite'
 const apiUrl = loadEnv(process.env.VITE_API_URL, process.cwd());
 
 export default defineConfig({
-    server: {
-      cors: {
-        // the origin you will be accessing via browser
-        origin: apiUrl,
-      },
+  root: './',
+  server: {
+    cors: {
+      // the origin you will be accessing via browser
+      origin: apiUrl,
     },
-    build: {
-      // generate .vite/manifest.json in outDir
-      manifest: true,
-      rollupOptions: {
-        // overwrite default .html entry
-        input: '/src/main.js',
-      },
-    },
-  })
+  },
+  build: {
+    // generate .vite/manifest.json in outDir
+    manifest: true,
+  },
+})
